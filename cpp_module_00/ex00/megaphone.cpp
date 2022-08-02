c9d87751274c74e475355597d4bc2826839dcb48
Fab <fabio.venturini.1989@gmail.com>
2022-08-02 20:13:04 +0200

    MEGAPHONE!

---
+++ cpp_module_00/ex00/megaphone.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+void    ft_str_capitalizer(char *str)
+{
+    int i;
+
+    i = 0;
+    while (str[i])
+    {
+        if (str[i] >= 97 && str[i] <= 122)
+        {
+            str[i] -= 32;
+            std::cout << str[i];
+        }
+        else
+            std::cout << str[i];
+        i++;
+    }
+}
+
+int main(int ac, char *av[])
+{
+    int i;
+
+    if (ac == 1)
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+    else
+    {
+        i = 1;
+        while (av[i]) {
+            ft_str_capitalizer(av[i]);
+            i++;
+        }
+        std::cout << std::endl;
+    }
+    return 0;
+}
