617e2c6b4b34465d0e6ed0c39913964a0f105721
FateEvent <fabio.venturini.1989@gmail.com>
2022-09-03 16:56:48 +0200

    with cpp mod 00 I'm done

--- cpp_module_00/ex00/megaphone.cpp
+++ cpp_module_00/ex00/megaphone.cpp
@@ -2,36 +2,36 @@
 
 void    ft_str_capitalizer(char *str)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (str[i])
-    {
-        if (str[i] >= 97 && str[i] <= 122)
-        {
-            str[i] -= 32;
-            std::cout << str[i];
-        }
-        else
-            std::cout << str[i];
-        i++;
-    }
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+		{
+			str[i] -= 32;
+			std::cout << str[i];
+		}
+		else
+			std::cout << str[i];
+		i++;
+	}
 }
 
 int main(int ac, char *av[])
 {
-    int i;
+	int	i;
 
-    if (ac == 1)
-        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-    else
-    {
-        i = 1;
-        while (av[i]) {
-            ft_str_capitalizer(av[i]);
-            i++;
-        }
-        std::cout << std::endl;
-    }
-    return 0;
+	if (ac == 1 || ac > 2)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else if (ac == 2)
+	{
+		i = 1;
+		while (av[i]) {
+			ft_str_capitalizer(av[i]);
+			i++;
+		}
+		std::cout << std::endl;
+	}
+	return 0;
 }
---
+++ cpp_module_06/ex00/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+
+int	main()
+{
+	std::cout << atof()
+}
