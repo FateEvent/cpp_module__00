7c1dc81f4be150143e44500e218113e00aea7b0b
FateEvent <fabio.venturini.1989@gmail.com>
2022-09-13 18:15:40 +0200

    charcuter la stack

--- cpp_module_08/ex02/main.cpp
+++ cpp_module_08/ex02/main.cpp
@@ -5,16 +5,58 @@
 
 int main()
 {
-	MutantStack<int> mstack;
-	mstack.push(5); mstack.push(17);
+	MutantStack<int>	mstack;
+
+	mstack.push(5);
+	mstack.push(17);
+
 	std::cout << mstack.top() << std::endl; mstack.pop();
+	
 	std::cout << mstack.size() << std::endl;
-	mstack.push(3); mstack.push(5); mstack.push(737); //[...] mstack.push(0);
-	MutantStack<int>::iterator it = mstack.begin(); MutantStack<int>::iterator ite = mstack.end();
+	
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	//[...]
+	mstack.push(0);
+	
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+	
 	++it;
 	--it;
-	while (it != ite) {
+	while (it != ite)
+	{
 			std::cout << *it << std::endl;
-	++it; }
-	std::stack<int> s(mstack); return 0;
+	++it;
+	}
+
+	MutantStack<int>	mlist;
+
+	mlist.push(5);
+	mlist.push(17);
+
+	std::cout << mlist.top() << std::endl; mlist.pop();
+	
+	std::cout << mlist.size() << std::endl;
+	
+	mlist.push(3);
+	mlist.push(5);
+	mlist.push(737);
+	//[...]
+	mlist.push(0);
+	
+	MutantStack<int>::iterator it2 = mlist.begin();
+	MutantStack<int>::iterator it2e = mlist.end();
+	
+	++it2;
+	--it2;
+	while (it2 != it2e)
+	{
+			std::cout << *it2 << std::endl;
+	++it2;
+	}
+//	std::stack<int> s(mstack);
+//	std::stack<int> s(mlist);
+	return 0;
 }
