960aab297f9eb94a54f46fc15e72cf9a6de42bc1
Fabio Venturini <faventur@k0r3p2.42mulhouse.fr>
2023-07-18 11:12:34 +0200

    Allocations for diam in main.cpp

--- cpp_module_03/ex03/DiamondTrap.cpp
+++ cpp_module_03/ex03/DiamondTrap.cpp
@@ -1,28 +1,26 @@
 #include "DiamondTrap.hpp"
 
-DiamondTrap::DiamondTrap( void ) : ClapTrap("DiamC3P0_clap_name"), ScavTrap("default DiamC3P0"),
-	FragTrap("default DiamC3P0"), _name("default DiamC3P0")
+DiamondTrap::DiamondTrap( void ) : ClapTrap("DiamC3P0_clap_name"), _name("default DiamC3P0")
 {
 	_hitPoints = FragTrap::_hitPoints;
-	std::cout << _hitPoints << std::endl;
-	std::cout << FragTrap::_hitPoints << std::endl;
 	_energyPoints = ScavTrap::_energyPoints;
-	std::cout << _energyPoints << std::endl;
-	std::cout << ScavTrap::_energyPoints << std::endl;
 	_attackDamage = FragTrap::_attackDamage;
-	std::cout << _attackDamage << std::endl;
-	std::cout << FragTrap::_attackDamage << std::endl;
 	std::cout << "The DiamondTrap Mountain " << this->_name << " has risen from the depths of the RoboCorps factories!" << std::endl;
+	std::cout << "It possesses " << this->_hitPoints << " hit points, " << _energyPoints << " energy points and causes ";
+	std::cout << this->_attackDamage << " attack damages." << std::endl;
+	std::cout << std::endl;
 }
 
 
-DiamondTrap::DiamondTrap(std::string const name) : ClapTrap(name + "_clap_name"), ScavTrap(name),
-	FragTrap(name), _name(name)
+DiamondTrap::DiamondTrap(std::string const name) : ClapTrap(name + "_clap_name"), _name(name)
 {
 	_hitPoints = FragTrap::_hitPoints;
 	_energyPoints = ScavTrap::_energyPoints;
 	_attackDamage = FragTrap::_attackDamage;
 	std::cout << "The DiamondTrap Mountain " << this->_name << " has risen from the depths of the RoboCorps factories!" << std::endl;
+	std::cout << "It possesses " << this->_hitPoints << " hit points, " << _energyPoints << " energy points and causes ";
+	std::cout << this->_attackDamage << " attack damages." << std::endl;
+	std::cout << std::endl;
 }
 
 
@@ -31,13 +29,15 @@ DiamondTrap::DiamondTrap( const DiamondTrap &other ) : ClapTrap(other.getName() 
 	_hitPoints = FragTrap::_hitPoints;
 	_energyPoints = ScavTrap::_energyPoints;
 	_attackDamage = FragTrap::_attackDamage;
-  std::cout << "An upgraded version of the DiamondTrap Mountain " << this->_name << " has risen from the depths of the RoboCorps factories!" << std::endl;
+	std::cout << "An upgraded version of the DiamondTrap Mountain " << this->_name << " has risen from the depths of the RoboCorps factories!" << std::endl;
+	std::cout << std::endl;
 }
 
 
 DiamondTrap::~DiamondTrap( void )
 {
-    std::cout << "The DiamondTrap Mountain " << this->_name << " has fallen, sir!" << std::endl;
+		std::cout << "The DiamondTrap Mountain " << this->_name << " has fallen, sir!" << std::endl;
+		std::cout << std::endl;
 }
 
 
@@ -46,6 +46,7 @@ DiamondTrap &   DiamondTrap::operator = ( const DiamondTrap &other )
 	ClapTrap::_name = other.getName();
 	_name = other._name;
 	std::cout << "DiamondTrap " << _name << ": copy assignment operator called" << std::endl;
+	std::cout << std::endl;
 	return (*this);
 }
 
@@ -61,4 +62,5 @@ void    DiamondTrap::whoAmI( void )
 	std::cout << "The DiamondTrap Mountain " << this->_name << ", " << ClapTrap::getName() << " reporting for duty." << std::endl;
 	std::cout << "I have " << this->_hitPoints << " hit points, " << _energyPoints << " energy points and cause ";
 	std::cout << this->_attackDamage << " attack damages." << std::endl;
+	std::cout << std::endl;
 }
--- cpp_module_03/ex03/main.cpp
+++ cpp_module_03/ex03/main.cpp
@@ -6,27 +6,32 @@
 
 int main()
 {
-	DiamondTrap	fab( "Fab" );
-//	FragTrap	diane( "Diane" );
-	DiamondTrap	shanti( "Shanti" );
-	DiamondTrap	kay( shanti );
-	DiamondTrap	kitty;
-	DiamondTrap	bruce;
+	DiamondTrap	*fab = new DiamondTrap( "Fab" );
+	FragTrap	diane( "Diane" );
+	DiamondTrap	*shanti = new DiamondTrap( "Shanti" );
+	DiamondTrap	*kay = new DiamondTrap( *shanti );
+	DiamondTrap	*kitty = new DiamondTrap;
+	DiamondTrap	*bruce = new DiamondTrap;
 
-	bruce.whoAmI();
-//	fab.attack( "Shanti" );
-//	diane.attack( "Fab" );
-	fab.takeDamage( 30 );
-//	diane.beRepaired( 100 );
-//	diane.attack( "Fab" );
-//	shanti.attack( "Diane" );
-//	shanti.whoAmI();
-	bruce.takeDamage(68);
-	std::cout << "The robot " << bruce.getName() << " has " << bruce.getHitPoints() << " hit points left." << std::endl;
-	bruce = fab;
-	std::cout << "The robot " << bruce.getName() << " has " << bruce.getHitPoints() << " hit points left." << std::endl;
-	std::cout << "The robot " << kitty.getName() << " has " << kitty.getHitPoints() << " hit points left." << std::endl;
-	std::cout << "The robot " << kay.getName() << " has " << kay.getHitPoints() << " hit points left." << std::endl;
+	// fab->highFivesGuys();
+	bruce->whoAmI();
+	fab->whoAmI();
+	fab->attack( "Shanti" );
+	// diane.attack( "Fab" );
+	fab->takeDamage( 30 );
+	// fab->guardGate();
+	// diane.beRepaired( 100 );
+	// diane.attack( "Fab" );
+	shanti->attack( "Diane" );
+	shanti->whoAmI();
+	bruce->takeDamage(68);
+	std::cout << "The robot " << fab->getName() << " has " << fab->getHitPoints() << " hit points left." << std::endl;
+	std::cout << "The robot " << bruce->getName() << " has " << bruce->getHitPoints() << " hit points left." << std::endl;
+	*bruce = *fab;
+	std::cout << "The robot " << bruce->getName() << " has " << bruce->getHitPoints() << " hit points left." << std::endl;
+	// std::cout << "The robot " << kitty->getName() << " has " << kitty->getHitPoints() << " hit points left." << std::endl;
+	// std::cout << "The robot " << kay->getName() << " has " << kay->getHitPoints() << " hit points left." << std::endl;
 
+	delete fab;
 	return (0);
 }
